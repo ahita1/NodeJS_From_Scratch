7473d2fa3b33a40819d096c0ecf863fd1e256131
ahita1 <ahitafani833@gmail.com>
2023-11-23 23:35:47 +0300

    Add new features haha

--- express_demo/index.js
+++ express_demo/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const users = [1,2,3,4,5,6,7,8]
+// const port = 3000
 
-app.get('api/courese', (req, res) =>{
-    console.log(res.json(users));
+app.get('/', (req, res) =>{
+    res.send("hello Ahita, how are you haha")
 })
 
-app.listen(port, () => {
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3,4,5,6,7])
+})
+
+app.listen(3000 , () => {
     console.log("server is listening on port 3000...");
 })
